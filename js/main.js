c369abaaa5edc195946832ee30af5dafb02c4053
mac <ojekunleadetunji@gmail.com>
2021-08-01 00:17:46 +0100

    Adds more data from API

--- js/main.js
+++ js/main.js
@@ -1,82 +1,96 @@
 //New decks of cards are gotten at refresh
 
-const freshDecksUrl = 'https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1';
-let freshDecksDeckId =''
+const freshDecksUrl =
+  "https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1";
+let freshDecksDeckId = "";
 fetch(freshDecksUrl)
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-    console.log(data)
-    freshDecksDeckId = data.deck_id
+  .then((res) => res.json()) // parse response as JSON
+  .then((data) => {
+    console.log(data);
+    freshDecksDeckId = data.deck_id;
   })
-  .catch(err => {
-      console.log(`error ${err}`)
-});
+  .catch((err) => {
+    console.log(`error ${err}`);
+  });
 
+document.querySelector("#drawCard").addEventListener("click", drawDeck);
+document.querySelector("#reshuffle").addEventListener("click", reshuffle);
 
-document.querySelector('#drawCard').addEventListener('click', drawDeck)
-document.querySelector('#reshuffle').addEventListener('click', reshuffle)
+let p1 = 0;
+let p2 = 0;
 
+function drawDeck() {
+  const url = `https://deckofcardsapi.com/api/deck/${freshDecksDeckId}/draw/?count=2`;
+  let playerOne = "";
+  let playerTwo = "";
 
-function drawDeck(){
-  const url = `https://deckofcardsapi.com/api/deck/${freshDecksDeckId}/draw/?count=2`
-  let playerOne = ''
-  let playerTwo = ''
   fetch(url)
-      .then(res => res.json()) // parse response as JSON
-      .then(data => {
-        console.log(data)
-        playerOne= data.cards[0];
-        playerTwo = data.cards[1];
-        document.querySelector('#remaining').innerHTML= data.remaining;
-        document.querySelector('#playerOne').src= playerOne.image;
-        document.querySelector('#playerTwo').src= playerTwo.image;
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      // console.log(data);
+      console.log(p1, p2);
+      playerOne = data.cards[0];
+      playerTwo = data.cards[1];
+      document.querySelector("#remaining").innerHTML = data.remaining;
+      document.querySelector("#playerOne").src = playerOne.image;
+      document.querySelector("#playerTwo").src = playerTwo.image;
+      document.querySelector("#p1").innerHTML = p1;
+      document.querySelector("#p2").innerHTML = p2;
 
-        function valueChecker(value){
-          if(value=='ACE'){
-            return 11
-          }else if(value == 'KING'){
-            return 14
-          }else if(value == 'QUEEN'){
-            return 13
-          }else if(value == 'JACK'){
-            return 12
-          }else{
-            return value
-          }
+      function valueChecker(value) {
+        if (value == "ACE") {
+          return 11;
+        } else if (value == "KING") {
+          return 14;
+        } else if (value == "QUEEN") {
+          return 13;
+        } else if (value == "JACK") {
+          return 12;
+        } else {
+          return value;
         }
-
-        let playerOneValue =Number(valueChecker(data.cards[0].value))
-        let playerTwoValue =Number(valueChecker(data.cards[1].value))
-
-        
-
-        if(playerOneValue>playerTwoValue){
-          return document.querySelector('#winner').innerHTML="Player One Won"
-        }
-        else if(playerTwoValue>playerOneValue){
-          return document.querySelector('#winner').innerHTML="Player Two Won"
-        }else{
-          return document.querySelector('#winner').innerHTML="War"
-        }
-
+      }
 
       
 
-      })
-      .catch(err => {
-          console.log(`error ${err}`)
-      });
+      let playerOneValue = Number(valueChecker(data.cards[0].value));
+      let playerTwoValue = Number(valueChecker(data.cards[1].value));
+
+      if (playerOneValue > playerTwoValue) {
+        p1 += 1;
+        return (document.querySelector("#winner").innerHTML = "Computer +1");
+      } else if (playerTwoValue > playerOneValue) {
+        p2 += 1;
+        return (document.querySelector("#winner").innerHTML = "You +1");
+      } else {
+        return (document.querySelector("#winner").innerHTML = "War");
+      }
+
+      
+
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
 
-const reshuffleUrl = `https://deckofcardsapi.com/api/deck/${freshDecksDeckId}/shuffle/`
-function reshuffle(){
+const reshuffleUrl = `https://deckofcardsapi.com/api/deck/${freshDecksDeckId}/shuffle/`;
+function reshuffle() {
   fetch(freshDecksUrl)
-  .then(res => res.json()) // parse response as JSON
-  .then(data => {
-    console.log(data)
-    freshDecksDeckId = data.deck_id
-  })
-  .catch(err => {
-      console.log(`error ${err}`)
-});
+    .then((res) => res.json()) // parse response as JSON
+    .then((data) => {
+      console.log(data);
+      freshDecksDeckId = data.deck_id;
+      p1 = 0;
+      p2 = 0;
+      document.querySelector("#remaining").innerHTML = data.remaining;
+      document.querySelector("#winner").innerHTML = " ";
+      document.querySelector("#playerOne").src = null;
+      document.querySelector("#playerTwo").src = null;
+      document.querySelector("#p1").innerHTML = p1;
+      document.querySelector("#p2").innerHTML = p2;
+    })
+    .catch((err) => {
+      console.log(`error ${err}`);
+    });
 }
